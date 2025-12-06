6465075bd6c90ded7d71d3487c71ba4659e0475c
melrose312 <melrose312@gmail.com>
2025-12-05 16:48:22 -0800

    Featured Section Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
+import Featured from "./components/Featured";
 import Highlights from "./components/Highlights";
 
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 
-
-
-
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }

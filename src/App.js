b4fc0fb7cdc44207ea48e1a3cbc009d106330025
melrose312 <melrose312@gmail.com>
2025-12-06 09:50:37 -0800

    Links Added, in Progress

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import Featured from "./components/Featured";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-import Discounted from "./components/Discounted";
-
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data"
+import BooksInfo from "./pages/BookInfo"
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+         <Route path="/books" element={<Books books={books} />} />
+         <Route path="/books/:id" element={<BooksInfo books={books} />} />
+        </Routes>
+        {/* <Home /> */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

da1604d306d827de56c3a941af1c82faaa0697e6
melrose312 <melrose312@gmail.com>
2025-12-06 12:57:59 -0800

    Cart Logic in Progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data"
 import BooksInfo from "./pages/BookInfo"
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
+
   return (
     <Router>
       <div className="App">
@@ -14,9 +28,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
          <Route path="/books" element={<Books books={books} />} />
-         <Route path="/books/:id" element={<BooksInfo books={books} />} />
+         <Route path="/books/:id" element={<BooksInfo books={books} addToCart={addToCart} cart={cart} />} />
+         <Route path="/cart" element={<Cart books={books} />} />
         </Routes>
-        {/* <Home /> */}
         <Footer />
       </div>
     </Router>
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,12 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Rating from "../components/ui/Rating";
 import Price from "../components/ui/Price";
 import Book from "../components/ui/Book";
 
-const BookInfo = ({ books }) => {
-    const { id }= useParams();
-    const book = books.find(book => +book.id === +id);
+const BookInfo = ({ books, addToCart }) => {
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+  const [added, setAdded] = useState(false);
+
+  function addBookToCart(book) {
+    setAdded(true);
+    addToCart(book);
+  }
+  
 
   return (
     <div id="books__body">
@@ -23,53 +31,58 @@ const BookInfo = ({ books }) => {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img
-                  src={book.url}
-                  className="book__selected--img"
-                />
+                <img src={book.url} className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                  {book.title}
-                </h2>
+                <h2 className="book__selected--title">{book.title}</h2>
                 <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                  <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
                 </div>
                 <div className="book__summary">
-                    <h3 className="book__summary--title">
-                        Summary
-                    </h3>
-                    <p className="book__summary--para">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat non atque error laudantium vero deleniti ipsum nostrum enim assumenda, saepe quam dolorem impedit quisquam, praesentium dolore repellendus suscipit ea quas.
-                    </p>
-                    <p className="book__summary--para">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat non atque error laudantium vero deleniti ipsum nostrum enim assumenda, saepe quam dolorem impedit quisquam, praesentium dolore repellendus suscipit ea quas.
-                    </p>
+                  <h3 className="book__summary--title">Summary</h3>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellat non atque error laudantium vero deleniti ipsum
+                    nostrum enim assumenda, saepe quam dolorem impedit quisquam,
+                    praesentium dolore repellendus suscipit ea quas.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellat non atque error laudantium vero deleniti ipsum
+                    nostrum enim assumenda, saepe quam dolorem impedit quisquam,
+                    praesentium dolore repellendus suscipit ea quas.
+                  </p>
                 </div>
-                <button className="btn">
+
+                {added ? (
+                  <button className="btn">Checkout</button>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
                     Add to Cart
-                </button>
+                  </button>
+                )}
               </div>
             </div>
           </div>
         </div>
         <div className="books__container">
-            <div className="row">
-                <div className="book__selected--top">
-                    <h2 className="book__selected--title--top">
-                        Recommended Books
-                    </h2>
-                </div>
-                <div className="books">
-            {
-              books
-              .filter(book => book.rating === 5 && +book.id !== +id)
-              .slice(0, 4) 
-              .map(book => <Book book={book} key={book.id} />) 
-            }
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
             </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && +book.id !== +id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
             </div>
+          </div>
         </div>
       </main>
     </div>

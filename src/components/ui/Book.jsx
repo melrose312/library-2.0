378006c442f4d6debbb22a8661f63b30812db0d0
melrose312 <melrose312@gmail.com>
2025-12-07 10:24:21 -0800

    Loading State Added, Tested

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,24 +1,59 @@
-
-import { Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
 
-
 const Book = ({ book }) => {
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    let isMounted = true;
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout (() => {
+        if (isMounted) {
+          setImg(image);
+        }  
+      }, 300);
+    };
+    return () => {
+      // When the component unmounts
+      isMounted = false;
+    }
+  }, [book.url]);
+
   return (
     <div className="book">
-      <Link to={`/books/${book.id}`}>
-        <figure className="Book__img--wrapper">
-          <img src={book.url} alt="" className="book__img" />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title--link">
-          {book.title}
-        </Link>
-      </div>
-     <Rating rating={book.rating} />
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img src={img.src} 
+              alt="" 
+              className="book__img" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            salePrice={book.salePrice}
+            originalPrice={book.originalPrice}
+          />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton">
+            <div className="skeleton__title--skeleton"></div>
+            <div className="skeleton__rating--skeleton"></div>
+            <div className="skeleton__price--skeleton"></div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
